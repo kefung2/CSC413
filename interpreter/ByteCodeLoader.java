6cae1b85a9c41599321fb86a60af428cab481988
Anthony J Souza <ajsouza88@gmail.com>
2018-09-21 22:17:07 -0700

    committing clean base code

---
+++ interpreter/ByteCodeLoader.java
@@ -0,0 +1,33 @@
+
+package interpreter;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class ByteCodeLoader extends Object {
+
+    private BufferedReader byteSource;
+    
+    /**
+     * Constructor Simply creates a buffered reader.
+     * YOU ARE NOT ALLOWED TO READ FILE CONTENTS HERE
+     * THIS NEEDS TO HAPPEN IN LOADCODES.
+     */
+    public ByteCodeLoader(String file) throws IOException {
+        this.byteSource = new BufferedReader(new FileReader(file));
+    }
+    /**
+     * This function should read one line of source code at a time.
+     * For each line it should:
+     *      Tokenize string to break it into parts.
+     *      Grab THE correct class name for the given ByteCode from CodeTable
+     *      Create an instance of the ByteCode class name returned from code table.
+     *      Parse any additional arguments for the given ByteCode and send them to
+     *      the newly created ByteCode instance via the init function.
+     */
+    public Program loadCodes() {
+       return null;
+    }
+}
---
+++ interpreter/VirtualMachine.java
@@ -0,0 +1,17 @@
+package interpreter;
+
+import java.util.Stack;
+
+public class VirtualMachine {
+
+    private RunTimeStack runStack;
+    private Stack returnAddrs;
+    private Program program;
+    private int pc;
+    private boolean isRunning;
+
+    protected VirtualMachine(Program program) {
+        this.program = program;
+    }
+
+}
