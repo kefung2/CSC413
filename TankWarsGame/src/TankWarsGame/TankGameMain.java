ceab7b811ced2390680c05d39ab45fb36a8a8382
Kevin <kevinfung9622@gmail.com>
2019-03-12 21:35:55 -0700

    add file

--- TankWarsGame/src/TankWarsGame/TankGameMain.java
+++ TankWarsGame/src/TankWarsGame/TankGameMain.java
@@ -1,10 +1,7 @@
 package TankWarsGame;
 
 public class TankGameMain {
-    public static void main(String args[]) {
-
-        System.out.println("hi");
-        //test
-
+    public static void main(String args[]){
+        System.out.println("test 2");
     }
 }
---
+++ TankWarsGame/src/TankWarsGame/Tanks.java
@@ -0,0 +1,5 @@
+package TankWarsGame;
+
+public class Tanks {
+
+}
